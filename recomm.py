6bd5d54bf0a5ff1b2d91c47ad73d20db9a6b180d
Rodrimansidub14 <man22611@uvg.edu.gt>
2023-05-21 23:08:43 -0600

    implementacion de algoritmo y de menu principal

---
+++ recomm.py
@@ -0,0 +1,85 @@
+from conexion import run_query
+
+# Knowledge-based recommendation function
+def recommend_knowledge_based(user_preferences):
+    # Write a Cypher query to find tutors/resources based on user preferences
+    cypher_query = """
+    MATCH (tutor:Tutor)-[:TEACHES]->(subject:Subject)
+    WHERE subject.name IN {preferences}
+    RETURN tutor
+    UNION
+    MATCH (resource:Resource)-[:ABOUT]->(subject:Subject)
+    WHERE subject.name IN {preferences}
+    RETURN resource
+    """.format(preferences=user_preferences)
+    
+    results = run_query(cypher_query)
+    return results
+
+# User-based collaborative filtering function
+def recommend_user_based(user_id):
+    # Write a Cypher query to find similar users and what tutors/resources they like
+    cypher_query = """
+    MATCH (user:User)-[:LIKED]->(resource:Resource)<-[:LIKED]-(similarUser:User)
+    WHERE user.id = {user_id} AND NOT (user)-[:LIKED]->(resource)
+    RETURN resource
+    """.format(user_id=user_id)
+    
+    results = run_query(cypher_query)
+    return results
+
+# Hybrid recommendation function
+def recommend(user_preferences, user_id):
+    knowledge_based_results = recommend_knowledge_based(user_preferences)
+    user_based_results = recommend_user_based(user_id)
+    
+    # Here you'd combine the results from the two methods in a way that makes sense for your use case
+    # This is just a simple example
+    combined_results = list(set(knowledge_based_results + user_based_results))
+    
+    return combined_results
+def main_menu():
+    print("Bienvenido al portal de tutorias")
+    print("1. Tutorias")
+    print("2. Otro tipo")
+    choice = input("elige una opción: ")
+    
+    if choice == '1':
+        tutor_menu()
+    elif choice == '2':
+        other_resources_menu()
+    else:
+        print("Invalid option")
+        main_menu()
+
+def tutor_menu():
+    print("Selecciona el tipo de tutorias")
+    print("1. Virtuales")
+    print("2. Presenciales")
+    choice = input("Choose an option: ")
+
+    year = input("¿Que año cursas actualmente? : ")
+    semester = input("¿En que semestre te encuentras?: ")
+    course = input("Ingresa el curso en el que necesitas apoyo: ")
+
+    from recomm import recommend_knowledge_based
+    tutors = recommend_knowledge_based([course])
+    print("Recommended tutors:", tutors)
+
+def other_resources_menu():
+    choice = input("Necesitas videos? (si/no): ")
+
+    if choice.lower() == 'si':
+        # Here, call the recommend function with the appropriate parameters
+        from recomm import recommend_knowledge_based
+        videos = recommend_knowledge_based(["Video Topic 1", "Video Topic 2"])  # Replace with actual video topics
+        print("Recommended videos:", videos)
+    else:
+        # Here, call the recommend function with the appropriate parameters
+        from recomm import recommend_knowledge_based
+        other_resources = recommend_knowledge_based(["Non-Video Topic 1", "Non-Video Topic 2"])  # Replace with actual non-video topics
+        print("Recommended resources:", other_resources)
+
+# Run the menu
+main_menu()
+
